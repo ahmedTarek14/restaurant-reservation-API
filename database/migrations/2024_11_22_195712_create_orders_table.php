afd8a17c72686ca9709738c956394fa101012931
ahmedTarek14 <ahmedtarek.am@gmail.com>
2024-11-22 22:04:51 +0200

    Initial commit for restaurant reservation API

---
+++ database/migrations/2024_11_22_195712_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('waiter_id')->nullable();
+            $table->decimal('total', 10, 2);
+            $table->boolean('paid')->default(false);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
